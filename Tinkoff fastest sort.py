ce046ca10bbc4aca1f3e90ed600cba45abd8e216
fllnrn <lazy@home.local>
2022-02-01 17:50:56 +0300

    fastest sort on almost sorted array

---
+++ Tinkoff fastest sort.py
@@ -0,0 +1,129 @@
+# вставками
+def insertion_sort(nums):  
+    # Сортировку начинаем со второго элемента, т.к. считается, что первый элемент уже отсортирован
+    for i in range(1, len(nums)):
+        item_to_insert = nums[i]
+        # Сохраняем ссылку на индекс предыдущего элемента
+        j = i - 1
+        # Элементы отсортированного сегмента перемещаем вперёд, если они больше
+        # элемента для вставки
+        while j >= 0 and nums[j] > item_to_insert:
+            nums[j + 1] = nums[j]
+            j -= 1
+        # Вставляем элемент
+        nums[j + 1] = item_to_insert
+
+#слиянием
+def merge(left_list, right_list):  
+    sorted_list = []
+    left_list_index = right_list_index = 0
+
+    # Длина списков часто используется, поэтому создадим переменные для удобства
+    left_list_length, right_list_length = len(left_list), len(right_list)
+
+    for _ in range(left_list_length + right_list_length):
+        if left_list_index < left_list_length and right_list_index < right_list_length:
+            # Сравниваем первые элементы в начале каждого списка
+            # Если первый элемент левого подсписка меньше, добавляем его
+            # в отсортированный массив
+            if left_list[left_list_index] <= right_list[right_list_index]:
+                sorted_list.append(left_list[left_list_index])
+                left_list_index += 1
+            # Если первый элемент правого подсписка меньше, добавляем его
+            # в отсортированный массив
+            else:
+                sorted_list.append(right_list[right_list_index])
+                right_list_index += 1
+
+        # Если достигнут конец левого списка, элементы правого списка
+        # добавляем в конец результирующего списка
+        elif left_list_index == left_list_length:
+            sorted_list.append(right_list[right_list_index])
+            right_list_index += 1
+        # Если достигнут конец правого списка, элементы левого списка
+        # добавляем в отсортированный массив
+        elif right_list_index == right_list_length:
+            sorted_list.append(left_list[left_list_index])
+            left_list_index += 1
+
+    return sorted_list
+
+def merge_sort(nums):  
+    # Возвращаем список, если он состоит из одного элемента
+    if len(nums) <= 1:
+        return nums
+
+    # Для того чтобы найти середину списка, используем деление без остатка
+    # Индексы должны быть integer
+    mid = len(nums) // 2
+
+    # Сортируем и объединяем подсписки
+    left_list = merge_sort(nums[:mid])
+    right_list = merge_sort(nums[mid:])
+
+    # Объединяем отсортированные списки в результирующий
+    return merge(left_list, right_list)
+
+#быстрая
+def partition(nums, low, high):  
+    # Выбираем средний элемент в качестве опорного
+    # Также возможен выбор первого, последнего
+    # или произвольного элементов в качестве опорного
+    pivot = nums[(low + high) // 2]
+    i = low - 1
+    j = high + 1
+    while True:
+        i += 1
+        while nums[i] < pivot:
+            i += 1
+
+        j -= 1
+        while nums[j] > pivot:
+            j -= 1
+
+        if i >= j:
+            return j
+
+        # Если элемент с индексом i (слева от опорного) больше, чем
+        # элемент с индексом j (справа от опорного), меняем их местами
+        nums[i], nums[j] = nums[j], nums[i]
+
+def quick_sort(nums):  
+    # Создадим вспомогательную функцию, которая вызывается рекурсивно
+    def _quick_sort(items, low, high):
+        if low < high:
+            # This is the index after the pivot, where our lists are split
+            split_index = partition(items, low, high)
+            _quick_sort(items, low, split_index)
+            _quick_sort(items, split_index + 1, high)
+
+    _quick_sort(nums, 0, len(nums) - 1)
+
+#пузырьковая
+def bubble_sort(nums):  
+    # Устанавливаем swapped в True, чтобы цикл запустился хотя бы один раз
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                # Меняем элементы
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Устанавливаем swapped в True для следующей итерации
+                swapped = True
+
+
+import time
+
+def checkTime(func):
+    nums = [i for i in range(500)]
+    nums[300] = 0
+    start = time.time()
+    func(nums)
+    end = time.time()
+    print(end-start, func)
+
+checkTime(insertion_sort)
+checkTime(merge_sort)
+checkTime(quick_sort)
+checkTime(bubble_sort)
